13a62c5ecc1cea12ad7bbe879b067f19c2184017
Mirah Gary <mirah.gary@gmail.com>
2017-12-15 17:53:52 +0000

    Implemented Part 2 to determine loop size

--- Day6/main.go
+++ Day6/main.go
@@ -1,8 +1,29 @@
 package main
 
 import (
-  fileParsing "github.com/MirahImage/AdventOfCode2017/FileParsing"
+	"fmt"
+
+	. "github.com/MirahImage/AdventOfCode2017/Day6/History"
+	. "github.com/MirahImage/AdventOfCode2017/Day6/Memory"
+	fileParsing "github.com/MirahImage/AdventOfCode2017/FileParsing"
 )
 
+const input = "input.txt"
+
+func CountReallocations(m Memory) (int, int) {
+	var h History
+	for h.Log(m) == -1 {
+		m.Reallocate()
+	}
+	return len(h.TimeSteps), len(h.TimeSteps) - h.Log(m)
+}
+
 func main() {
+	var m Memory
+	lines := fileParsing.ReadFileToLines(input)
+	m.Banks = fileParsing.LineToInts(lines[0])
+	fmt.Println("Initial state", m.Banks)
+	hist, loopSize := CountReallocations(m)
+	fmt.Println(hist, "reallocations to reach a repeated state")
+	fmt.Println("Loop is of size", loopSize)
 }
--- Day6/main_test.go
+++ Day6/main_test.go
@@ -5,8 +5,24 @@ import (
 	. "github.com/onsi/gomega"
 
 	. "github.com/MirahImage/AdventOfCode2017/Day6"
+	. "github.com/MirahImage/AdventOfCode2017/Day6/Memory"
 )
 
 var _ = Describe("Main", func() {
+	var (
+		m Memory
+	)
+
+	BeforeEach(func() {
+		m.Banks = []int{0, 2, 7, 0}
+	})
+
+	Describe("CountReallocations", func() {
+		It("should return the number of reallocations required to reach a repeated state", func() {
+			hist, loopSize := CountReallocations(m)
+			Expect(hist).To(Equal(5))
+			Expect(loopSize).To(Equal(4))
+		})
+	})
 
 })
