bb5852da56fdc91e1a7b732effebf63e3f030bb5
Mirah Gary <mirah.gary@gmail.com>
2017-12-15 17:33:13 +0000

    Refactored to use fileParsing.LineToInts

--- Day2/main.go
+++ Day2/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	. "github.com/MirahImage/AdventOfCode2017/Day2/row"
 	. "github.com/MirahImage/AdventOfCode2017/Day2/spreadsheet"
@@ -11,19 +10,10 @@ import (
 
 const input = "input.txt"
 
-func LineToInts(line string) []int {
-	numberStrings := strings.Split(line, "\t")
-	numbers, err := fileParsing.SliceAtoi(numberStrings)
-	if err != nil {
-		panic(err)
-	}
-	return numbers
-}
-
 func LinesToRows(lines []string) []Row {
 	var rows = make([]Row, len(lines))
 	for i, line := range lines {
-		rows[i] = Row{Entries: LineToInts(line)}
+		rows[i] = Row{Entries: fileParsing.LineToInts(line)}
 	}
 	return rows
 }
--- Day2/main_test.go
+++ Day2/main_test.go
@@ -10,20 +10,6 @@ import (
 
 var _ = Describe("Main", func() {
 
-	Describe("LineToInts", func() {
-		var (
-			tabSeparatedIntString string
-		)
-
-		BeforeEach(func() {
-			tabSeparatedIntString = "5\t1\t9\t5"
-		})
-
-		It("should return a slice of ints", func() {
-			Expect(LineToInts(tabSeparatedIntString)).To(Equal([]int{5, 1, 9, 5}))
-		})
-	})
-
 	Describe("LinesToRows", func() {
 		var (
 			lines        []string
