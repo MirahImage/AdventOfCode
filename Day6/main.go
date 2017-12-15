6dc7a8df48b91023c0276b91b816efb16495331e
Mirah Gary <mirah.gary@gmail.com>
2017-12-15 15:11:18 +0000

    Created Day 6 package and test suite

---
+++ Day6/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+  fileParsing "github.com/MirahImage/AdventOfCode2017/FileParsing"
+)
+
+func main() {
+}
---
+++ Day6/main_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/MirahImage/AdventOfCode2017/Day6"
+)
+
+var _ = Describe("Main", func() {
+
+})
